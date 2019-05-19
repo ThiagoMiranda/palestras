65676258075b818dc3712e3705b94161713561b5
Thiago Miranda <thiagomir@gmail.com>
2019-05-19 19:18:01 -0300

    teste

--- hooks-app/src/components/statelessForm/SLForm.js
+++ hooks-app/src/components/statelessForm/SLForm.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+function useWindowEffect() {
+    const [ windowWidth, setWindowWidth ] = useState(window.innerWidth)
+
+    useEffect(() => {
+        const onChangeWindowWidth = () => setWindowWidth(window.innerWidth)
+        window.addEventListener('resize', onChangeWindowWidth)
+        return () => window.removeEventListener('resize', onChangeWindowWidth)
+    }, [windowWidth])
+
+    return windowWidth
+}
+
+function useFormInput(initialValue) {
+    const [value, setValue] = useState(initialValue)
+
+    function onChangeCallback(evt) {
+        setValue(evt.target.value)
+    }
+
+    return {
+        value,
+        onChange: onChangeCallback
+    }
+}
 
 export default function StatefullForm() {
-    let company = '', cnpj = '', address = '', windowWidth = window.innerWidth
+    const company = useFormInput('')
+    const cnpj = useFormInput('')
+    const address = useFormInput('')
+    const windowWidth = useWindowEffect()
+
+    useWindowEffect()
 
     return (
         <div className='container'>
             <form className='form'>
                 <div className='input-wrapper'>
                     <label htmlFor='company'>Empresa:</label>
-                    <input name='company' id='company' value={company} placeholder='Digite o nome da empresa' onChange={() => {}} />
+                    <input name='company' id='company' placeholder='Digite o nome da empresa' {...company} />
 
                     <label htmlFor='cnpj'>CNPJ:</label>
-                    <input name='cnpj' id='cnpj' value={cnpj} placeholder='Digite o CNPJ' onChange={() => {}} />
+                    <input name='cnpj' id='cnpj' value={cnpj} placeholder='Digite o CNPJ' {...cnpj} />
 
                     <label htmlFor='address'>Endereço:</label>
-                    <input name='address' id='address' value={address} placeholder='Digite o endereço' onChange={() => {}} />
+                    <input name='address' id='address' placeholder='Digite o endereço' {...address} />
                     
                 </div>
                 <div className='input-wrapper footer'>
